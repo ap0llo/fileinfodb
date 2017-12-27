6d0a92a0cd2f4eabe27b7eae11dec07db548334a
Andreas Grünwald <andreasgruenwald@live.de>
2017-12-27 21:07:45 +0100

    Include --debug option in commandline help

--- src/FileInfoDb/main/Cli/ArgsBase.cs
+++ src/FileInfoDb/main/Cli/ArgsBase.cs
@@ -3,7 +3,13 @@
 namespace FileInfoDb.Cli
 {
     class ArgsBase
-    { 
+    {
+
+#if DEBUG
+        [Option("debug", HelpText = "Automatically attach debugger after launch")]
+        public bool AttachDebugger { get; set; }
+#endif 
+
         [Option("uri", Required= true)]
         public string DatabaseUri { get; set; }
     }
