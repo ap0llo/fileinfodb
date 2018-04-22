e65886056ad8ebdb220b3ee50e8e5b9fdda663e3
Andreas Grünwald <andreasgruenwald@live.de>
2018-04-22 20:47:31 +0200

    Add 'Index' command

---
+++ src/FileInfoDb/main/Cli/IndexArgs.cs
@@ -0,0 +1,21 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileInfoDb.Cli
+{
+    [Verb(CommandNames.Index, HelpText = "Compute and cache the hashes for files in a directory to speed up later " + 
+                                          CommandNames.GetProperty + " and " + CommandNames.SetProperty + 
+                                          " operations for these files")]
+    class IndexArgs : BaseArgs
+    {
+        [Option("directory", Required = true, HelpText = "The path of the directory to index")]
+        public string DirectoryPath { get; set; }
+
+        [Option('r', "recursive", HelpText = "Index all subdirectories, too")]
+        public bool Recursive { get; set; }
+    }
+}
