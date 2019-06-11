5fc95d8a25072c759341f2ea315f4741bcf3053b
Andreas Gruenwald <andreas@grynwald.net>
2019-06-11 19:45:39 +0200

    Add support for Visual Studio 2019 to msbuild.bat

--- msbuild.bat
+++ msbuild.bat
@@ -2,12 +2,23 @@
 SETLOCAL
 
 for /f "usebackq tokens=*" %%i in (`"%programfiles(x86)%\Microsoft Visual Studio\Installer\vswhere" -latest -products * -requires Microsoft.Component.MSBuild -property installationPath`) do (
-  set InstallDir=%%i
+  SET InstallDir=%%i
 )
-set MSBuildPath="%InstallDir%\MSBuild\15.0\Bin\MSBuild.exe"
+SET MSBuildPath="%InstallDir%\MSBuild\15.0\Bin\MSBuild.exe"
+
+IF NOT EXIST %MSBuildPath% (  
+  SET MSBuildPath="%InstallDir%\MSBuild\Current\Bin\MSBuild.exe"
+)
+
+IF NOT EXIST %MSBuildPath% (
+  ECHO No MSBuild installation found. Is Visual Studio 2015 or 2019 installed?
+  EXIT /B 1
+) 
+
+ECHO MSBuild found at %MSBuildPath%
 
 if /I "%APPVEYOR%" == "True" ( 
-    SET LOGGER=/logger:C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll
+    SET LOGGER=/loggit rger:C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll
 ) else (
     SET LOGGER=
 )
